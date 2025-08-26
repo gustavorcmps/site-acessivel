fb24e77ac7395206a2c8624d3f2b39ba82f36c9e
Gustavo Rosa <00001117522568SP@al.educacao.sp.gov.br>
2025-08-26 10:13:38 -0300

    Adicionado botão para diminuir a fonte

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 document.addEventListener("DOMContentLoaded", function () {
     const aumentaFonte = document.getElementById("fonte+");
+    const diminuiFonte = document.getElementById("fonte-");
+
     let fonteAtual = 1;
+
     aumentaFonte.addEventListener('click', function () {
         fonteAtual += 0.1;
+        document.body.style.fontSize = `${fonteAtual}rem`
+    })
+
+    diminuiFonte.addEventListener('click', function () {
+        fonteAtual -= 0.1;
         document.body.style.fontSize = `${fonteAtual}rem`
     })
 });
