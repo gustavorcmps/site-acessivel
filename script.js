29943d137946ee9153ea2755b9dd5e7967d439bb
Gustavo Rosa <00001117522568sp@al.educacao.sp.gov.br>
2025-08-30 20:07:52 -0300

    Alto contraste adicionado

--- script.js
+++ script.js
@@ -1,24 +1,29 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const acessibilidade = document.getElementById('botaoMenu');
-    const opcoes = document.getElementById('opcoesMenu')
+  const acessibilidade = document.getElementById("botaoMenu");
+  const opcoes = document.getElementById("opcoesMenu");
 
-    acessibilidade.addEventListener('click', function(){
-        acessibilidade.classList.toggle('rotation')
-        opcoes.classList.toggle('apresentaMenu')
-    })
+  acessibilidade.addEventListener("click", function () {
+    acessibilidade.classList.toggle("rotation");
+    opcoes.classList.toggle("apresentaMenu");
+  });
 
-    const aumentaFonte = document.getElementById("fonte+");
-    const diminuiFonte = document.getElementById("fonte-");
+  const aumentaFonte = document.getElementById("fonte+");
+  const diminuiFonte = document.getElementById("fonte-");
+  const mudaContraste = document.getElementById("contraste");
 
-    let fonteAtual = 1;
+  let fonteAtual = 1;
 
-    aumentaFonte.addEventListener('click', function () {
-        fonteAtual += 0.1;
-        document.body.style.fontSize = `${fonteAtual}rem`
-    })
+  aumentaFonte.addEventListener("click", function () {
+    fonteAtual += 0.1;
+    document.body.style.fontSize = `${fonteAtual}rem`;
+  });
 
-    diminuiFonte.addEventListener('click', function () {
-        fonteAtual -= 0.1;
-        document.body.style.fontSize = `${fonteAtual}rem`
-    })
+  diminuiFonte.addEventListener("click", function () {
+    fonteAtual -= 0.1;
+    document.body.style.fontSize = `${fonteAtual}rem`;
+  });
+
+  mudaContraste.addEventListener("click", function () {
+    document.body.classList.toggle("contraste");
+  });
 });
