3d168a17e8771542f65b872bb3e7552dab418d01
Gustavo Rosa <00001117522568sp@al.educacao.sp.gov.br>
2025-09-04 18:58:06 -0300

    Animações adicionadas na página

--- script.js
+++ script.js
@@ -31,3 +31,8 @@ document.addEventListener("DOMContentLoaded", function () {
     document.body.classList.toggle("contraste");
   });
 });
+
+ScrollReveal().reveal("#inicio", { delay: 500 });
+ScrollReveal().reveal("#linkinpark", { delay: 500 });
+ScrollReveal().reveal("#galeria", { delay: 500 });
+ScrollReveal().reveal("#contato", { delay: 500 });
